612dce66b0a72e14a06e360932e7f8cda6bf52ff
JamesBuehlmann8 <113567904+JamesBuehlmann8@users.noreply.github.com>
2022-09-15 20:17:23 -0500

    Add files via upload

---
+++ Computer Science 101 Program #1.py
@@ -0,0 +1,26 @@
+print('***APPLE PIE INGREDIENTS***')
+print()
+pie_amnt = int(input('How many pies do you need? ==> '))
+print()
+print('For', pie_amnt, 'pie(s) you will need:')
+print()
+crust_amnt = pie_amnt
+apple_amnt = pie_amnt * 6
+sugar_amnt = pie_amnt * 0.75
+flour_amnt = pie_amnt * 2
+cinn_amnt = pie_amnt * 0.75
+salt_amnt = pie_amnt * 0.25
+nutmeg_amnt = pie_amnt * 0.125
+lemon_amnt = pie_amnt
+print(crust_amnt, 'Pie crust(s)')
+print(apple_amnt, 'Apples')
+print(sugar_amnt, 'cups of Sugar')
+print(flour_amnt, 'Tablespoon of Flour')
+print(cinn_amnt, 'Teaspoon of Cinnamon')
+print(salt_amnt, 'Teaspoon of Salt')
+print(nutmeg_amnt, 'Teaspoon of Nutmeg')
+print(lemon_amnt, 'Tablespoon of Lemon Juice')
+print()
+print('>>>')
+
+
